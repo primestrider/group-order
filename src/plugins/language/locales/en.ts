bd385be10cf3f263df75089ee2e75ccec6982845
Ricky Ng <ricky@aptika.co.id>
2025-12-10 13:32:34 +0700

    removed examples

--- src/plugins/language/locales/en.ts
+++ src/plugins/language/locales/en.ts
@@ -1,11 +1,8 @@
-import example from "@/features/example/languages/example.en"
-import utils from "@/shared/languages/utils.id"
 import order from "@/features/order/languages/order.en"
+import utils from "@/shared/languages/utils.id"
 
 export default {
   features: {
-    example,
-  
     order,
   },
   utils,
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-import exampleRoutes from "@/features/example/routes"
+import orderRoutes from "@/features/order/routes"
 import utilRoutes from "@/shared/routes"
-import orderRoutes from '@/features/order/routes'
 
-const listRoutes = [...exampleRoutes, ...utilRoutes,
-  ...orderRoutes,
-]
+const listRoutes = [...utilRoutes, ...orderRoutes]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
