bc2c7222f83455d9a8b7feb39b2c10bad17397a1
Ricky Ng <ricky@aptika.co.id>
2025-12-15 10:53:27 +0700

    add base form field and date formatter

--- src/shared/models/index.ts
+++ src/shared/models/index.ts
@@ -1,3 +1,5 @@
+import type { DATE_FNS_LOCALES } from "../helpers/date"
+
 export enum UtilsPageName {
   PAGE_NOT_FOUND = "PageNotFound",
 }
@@ -9,3 +11,37 @@ export type ButtonProps = {
   variant?: "primary" | "secondary"
   fluid?: boolean
 }
+
+export type BaseFormFieldProps = {
+  /**
+   * Field label text
+   */
+  label?: string
+
+  /**
+   * Field name / id (used for label-for & accessibility)
+   */
+  fieldName?: string
+
+  /**
+   * Error message from vee-validate or backend
+   */
+  error?: string | null
+
+  /**
+   * Mark field as required (UI only)
+   */
+  required?: boolean
+
+  /**
+   * Optional helper / hint text
+   */
+  hint?: string
+
+  /**
+   *  Disable Field
+   */
+  disabled?: boolean
+}
+
+export type DateLocaleCode = keyof typeof DATE_FNS_LOCALES
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -3,12 +3,6 @@ import type { RouteRecordRaw } from "vue-router"
 import { UtilsPageName } from "../models"
 
 const utilRoutes: RouteRecordRaw[] = [
-  // delete or change this later
-  {
-    path: "/landing",
-    name: "LandingPage",
-    component: () => import("@/shared/views/LandingPageView.vue"),
-  },
   {
     path: "/:pathMatch(.*)*",
     name: UtilsPageName.PAGE_NOT_FOUND,
